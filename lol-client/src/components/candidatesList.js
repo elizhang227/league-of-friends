15fecec8ccce613c02e83bad0aff12fecce24f02
elizhang227 <zhang.eli@hotmail.com>
2019-07-15 09:23:32 -0400

    added candidates list component

---
+++ lol-client/src/components/candidatesList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CandidatesList extends Component {
+    state = {
+        candidates: []
+    }
+
+    render() {
+        return (
+            <p>hi</p>
+        )
+    }
+
+}
+
+export default CandidatesList;
